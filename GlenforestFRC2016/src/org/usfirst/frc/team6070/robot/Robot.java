2db9f875e190927cf4d0bcd97d058733a32aac64
nafehshoaib <NafehShoaib@Nafehs-MacBook-Pro.local>
2016-02-18 16:34:17 -0500

    Autonomous Code and Horizontal and Vertical Sliders

--- GlenforestFRC2016/src/org/usfirst/frc/team6070/robot/Robot.java
+++ GlenforestFRC2016/src/org/usfirst/frc/team6070/robot/Robot.java
@@ -10,10 +10,14 @@ public class Robot extends IterativeRobot {
 	Victor BL = new Victor (3);
 	Victor BR = new Victor (2);
 	RobotDrive drive = new RobotDrive(FR, FL, BR, BL);
-	Victor v1 = new Victor (4);
+	Joystick joy = new Joystick (0);
+	boolean horiz = false;
+	boolean twopress = false;
+	
+	/*Victor v1 = new Victor (4);
 	Victor v2 = new Victor (5);
 	Joystick joy = new Joystick (0);
-	Joystick netjoy = new Joystick (1);
+	Joystick netjoy = new Joystick (1);*/
 
     public void robotInit() {
        
@@ -28,19 +32,59 @@ public class Robot extends IterativeRobot {
      * This function is called periodically during autonomous
      */
     public void autonomousPeriodic() {
-    	
+    	double speed = 0.5;
+    	double seconds = 1;
+    	FR.set(speed);
+    	FL.set(speed);
+    	BL.set(speed);
+    	BR.set(speed);
+    	Timer.delay(seconds);
+    	FR.set(-speed);
+    	FL.set(-speed);
+    	BL.set(-speed);
+    	BR.set(-speed);
+    	Timer.delay(0.4);
+    	FR.set(0);
+    	FL.set(0);
+    	BL.set(0);
+    	BR.set(0);
+    	Timer.delay(9.2);
     }
 
     /**
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
-        v2.set(joy.getX()+joy.getY());
-    	v1.set(joy.getX()-joy.getY());
-    	if (joy.getX() > 0.1 && joy.getX() < -0.1 && joy.getY() > 0.1 && joy.getY() < -0.1){
-    		drive.mecanumDrive_Cartesian(joy.getX(), joy.getY(), joy.getZ(), 0);
-    	}	
-    	if (netjoy.getY() > 0.1)
+        /*v2.set(joy.getX()+joy.getY());
+    	v1.set(joy.getX()-joy.getY());*/
+    	
+    	
+    	if (joy.getX() > 0.1 && joy.getX() < -0.1 && joy.getY() > 0.1 && joy.getY() < -0.1)
+    	{
+    		if (!horiz)
+    		{
+    			drive.mecanumDrive_Cartesian(joy.getX(), joy.getY(), joy.getZ(), 0);
+    		}
+    		else
+    		{
+    			drive.mecanumDrive_Cartesian(joy.getY(), joy.getX(), joy.getZ(), 0);
+    		}
+    		if (joy.getTrigger() && !twopress)
+    		{
+    			horiz = !horiz;
+    			twopress = true;
+    			if (horiz)
+    				System.out.println("Horizontal mode initiated.");
+    			else
+    				System.out.println("Vertical mode initiated.");
+    		}
+    		if (!joy.getTrigger()) {
+    			twopress = false;
+    		}
+    	}
+    	
+    	
+    	/*if (netjoy.getY() > 0.1)
     	{
     		v1.set(0.5);
     		v2.set(-0.5);
@@ -49,7 +93,7 @@ public class Robot extends IterativeRobot {
     	{
     		v1.set(-0.5);
     		v2.set(0.5);
-    	}
+    	}*/
     	Timer.delay(0.01);
     }
     
